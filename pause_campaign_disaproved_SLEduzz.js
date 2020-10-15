a9f1981b6ec6f462b1060647c1a3679338ed03f6
felipepradoSL <felipe@sweetleads.com.br>
2020-10-15 12:21:39 -0300

    Pausa campanha com anuncio reprovado e coloca na planilha

---
+++ pause_campaign_disaproved_SLEduzz.js
@@ -0,0 +1,184 @@
+/****************************************************************************
+**
+**  Sweet Leads Empreendimentos Digitais
+**  https://sweetleads.com.br
+**  felipe@sweetleads.com.br
+**
+**  GitHub
+**  https://github.com/felipepradoSL/script_ads_disapproved
+**
+**  Script verifica todos os anuncios que foram reprovados, pausa a campanha 
+**  e insere as campanhas na planilha, após isso é enviado um email de alerta
+**
+**  
+**  Planilha com as campanhas que foram pausadas:
+**  - https://docs.google.com/spreadsheets/d/1lL4sTacQFtlGS2vwpjmKSy6j4ppLvl84f5UfGkQywI4/
+**  
+**   SL Eduzz
+**  
+**
+*****************************************************************************/
+
+
+//arguments
+ADS_CONDITIONS =
+["CampaignStatus = ENABLED"
+,"AdGroupStatus = ENABLED"
+,"Status = ENABLED"
+,"CombinedApprovalStatus = DISAPPROVED"
+];
+
+// change values
+NOTIF_EMAIL = "afiliados@sweetleads.com.br" // email 
+EMAIL_SUBJECT = "[GOOGLE ADS] - Anúncios REPROVADOS pausados" //subject
+
+SPREADSHEET_ID = "1lL4sTacQFtlGS2vwpjmKSy6j4ppLvl84f5UfGkQywI4";  //insert a new blank spreadsheet url 
+
+
+// SpreadSheet header
+
+var SHEET_REPORT_HEADER = [
+"Ads ID",
+"Campaign Name", 
+"Campaign ID",
+"Disapproval Reason"
+];
+
+
+function main() {
+
+  //prepareSpreadsheet(); // prepare spreadsheet and add header
+
+  var results = getAds(ADS_CONDITIONS).map(pauseCampaign).map(saveSpreadsheet); //get campaings with ads disapproved, pause it and save in spreadsheet
+  
+  Logger.log("*************results****************");
+  Logger.log(JSON.stringify(results))
+  Logger.log("*************results****************");
+
+  var shouldNotify = notNil(results) ? true : false; //check if exists campaing disapproved
+  
+  if(shouldNotify){//send email case exists campaign disapproved
+    var emailBody = composeEmail(results)
+    MailApp.sendEmail(NOTIF_EMAIL, EMAIL_SUBJECT, emailBody, { noReply: true });
+  }
+
+}
+
+//check is not null
+function notNil(xs){
+  return xs.length && xs.length !== 0;
+}
+
+
+//get campaigns with ads disapproved
+function getAds(conds) {
+
+  var ads = [];
+  
+  var rawAdsIt = AdsApp.ads();
+  
+  var adsIt = conds
+  .reduce(function(acc,cond){ return acc.withCondition(cond) }, rawAdsIt)
+  .get();
+  
+  
+  while(adsIt.hasNext()) {
+
+    var current = adsIt.next();
+    var campaign = current.getCampaign();
+    var policyTopic = current.getPolicyTopics();
+    var disapprovalReason = policyTopic.reduce(function(acc, obj){    
+      return obj.getName();
+    }, "");
+
+    var adsId = current.getId();
+
+    var adData = { campaign: campaign }//objeto com a campanha
+    adData['adsId'] = adsId;
+    adData['disapprovalReason'] = disapprovalReason;
+
+    ads.push(adData);   
+
+  }  
+
+  return ads
+}
+
+
+// pause campaign
+function pauseCampaign(obj){
+  obj.campaign.pause();
+  Utilities.sleep(2000);
+  
+  return obj;
+}
+
+
+
+function composeEmail(results){
+
+  var currentAccount = AdsApp.currentAccount();
+  var accountName = currentAccount.getName();
+  var accountId = currentAccount.getCustomerId();
+
+  var firstLine = "Conta: "+ accountName + " - " + accountId + " \n" + "\n" + "As seguintes campanhas tiveram anúncio REPROVADO e foram pausadas: "+ "\n";
+  
+  var body = results.reduce(function(acc,obj){
+    var campaignName = obj.campaign.getName();
+
+    //Logger.log(acc); 
+    //Logger.log(campaignName); 
+    
+    return acc += "Campanha: " + campaignName + "\n"    
+    
+  }, firstLine);  
+  
+   var footer = "\n\nAlgumas informações da campanha: https://docs.google.com/spreadsheets/d/"+ SPREADSHEET_ID + "/ \n\n";
+
+  
+  return body + "" + footer;
+}
+
+
+//prepare spreadsheet
+function prepareSpreadsheet() {
+
+  var ss = SpreadsheetApp.openById(SPREADSHEET_ID);//open google sheets by ID (URL)
+  var sheet = ss.getActiveSheet(); // select sheet actived
+  sheet.clear(); //remove earlies alerts
+  sheet.clearConditionalFormatRules(); // clear all conditional format rules in sheet
+  sheet.appendRow(SHEET_REPORT_HEADER); // append header
+}
+
+
+// save spreadsheet
+function saveSpreadsheet(results) {
+
+var ss = SpreadsheetApp.openById(SPREADSHEET_ID);//open google sheets by ID (URL)
+var sheet = ss.getActiveSheet(); // select sheet actived
+
+
+var issues = [];
+
+//push ads in array 
+var campaignName = results.campaign.getName();
+var campaignId = results.campaign.getId();
+var adsId = results.adsId;
+var disapprovalReason = results.disapprovalReason;
+
+issues.push([
+  adsId,
+  campaignName,
+  campaignId,
+  disapprovalReason
+  ]);  
+
+var lastRow = sheet.getLastRow();
+
+// write issues to sheet
+var range = sheet.getRange(lastRow+1, 1, issues.length, SHEET_REPORT_HEADER.length);
+range.setValues(issues);
+
+return results
+}
+
